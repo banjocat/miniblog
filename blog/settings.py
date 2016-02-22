1ba0b62bff14fe8506be142409cd671bd76464a5
banjocat <jackmuratore@gmail.com>
2016-02-21 23:47:59 -0500

    Add admin and __str__ methods to models
    To allow creation of blog posts

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9%gncodf8r8++@ixx(we%1^8z4hpp3o2@y5kw-bvq8&o5xo=+='
+SECRET_KEY = 'ua)-f((x=y-ord4rs!n95qk^d)n81!e*vm4p9pjum*fv!uqlsh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- posts/models.py
+++ posts/models.py
@@ -18,5 +18,11 @@ class Post(models.Model):
     tags = models.ManyToManyField('Tag')
     status = models.IntegerField(choices=POST_STATUS)
 
+    def __str__(self):
+        return self.title
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
