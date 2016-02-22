ed42ab6fe1b31b2b8a75705f42c3de8a5b94e3a6
banjocat <jackmuratore@gmail.com>
2016-02-22 18:22:04 -0500

    Debug mode if not in DOCKER
    The dockerfile will set the DJANGO_DOCKER value to 1
    This will turn off DEBUG mode for production

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# when set in docker it is production
+if os.getenv('DJANGO_DOCKER') != 1:
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
