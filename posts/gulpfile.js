ab9b6d2a74761a1fad1ab5d9475a3326abcbb3c9
banjocat <jackmuratore@gmail.com>
2016-02-22 01:00:35 -0500

    Add markdown to blog posts
    Otherwise it be pretty boring looking

--- posts/gulpfile.js
+++ posts/gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var rename = require('gulp-rename');
 
+var FILES = {
+  'js': [
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/markdown/lib/markdown.js'
+  ]
+}
 gulp.task('less', function() {
   return gulp.src('static/less/style.less')
   .pipe(less())
@@ -9,4 +16,10 @@ gulp.task('less', function() {
 
 gulp.task('watch', function() {
   gulp.watch('static/less/style.less', ['less']);
+})
+
+gulp.task('js', function() {
+  return gulp.src(FILES.js)
+  .pipe(rename({dirname: ''}))
+  .pipe(gulp.dest('static/js/'));
 })
